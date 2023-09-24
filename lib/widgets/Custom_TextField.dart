067466f56354847e9a80040bcb5bc9a8c2cc7626
zeyad mostafa <142641321+ZooZM@users.noreply.github.com>
2023-09-24 20:09:17 +0300

    add file const

--- lib/widgets/Custom_TextField.dart
+++ lib/widgets/Custom_TextField.dart
@@ -1,21 +1,36 @@
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({this.onChanged,super.key, this.text,});
+  const CustomTextField({
+    this.onChanged,
+    super.key,
+    this.text,
+  });
   final String? text;
   final Function(String)? onChanged;
-   
+
   @override
   Widget build(BuildContext context) {
     return TextField(
       onChanged: onChanged,
       decoration: InputDecoration(
-        alignLabelWithHint: true,
-        labelText: text,
-        border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(5),
-            borderSide: BorderSide(color: Colors.white)),
+         alignLabelWithHint: true,
+    labelText: text,
+    labelStyle: const TextStyle(color: Colors.blue),
+    
+    border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(5),
+        borderSide:const BorderSide(color: Colors.white)),
+        enabledBorder: enable(),
+        focusedBorder: enable(color: Colors.blue)
       ),
     );
   }
+}
+
+OutlineInputBorder enable({color=Colors.white}) {
+  return OutlineInputBorder(
+    borderRadius: BorderRadius.circular(5),
+        borderSide: BorderSide(color: color)
+  );
 }
